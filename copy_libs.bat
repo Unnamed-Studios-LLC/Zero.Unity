83d3d3f4dcf8a1498aab0bdb4530630912cf4a54
Tucker <5263190+Juiix@users.noreply.github.com>
2022-06-01 21:27:21 -0600

    BatchId refactor in bridge

--- copy_libs.bat
+++
@@ -1,5 +0,0 @@
-xcopy /s "..\Zero.Gdk\Zero.Game.Client\bin\Release\netstandard2.0\Zero.Game.Client.dll" "Runtime\lib\Zero.Game.Client.dll" /Y
-xcopy /s "..\Zero.Gdk\Zero.Game.Client\bin\Release\netstandard2.0\Zero.Game.Common.dll" "Runtime\lib\Zero.Game.Common.dll" /Y
-xcopy /s "..\Zero.Gdk\Zero.Game.Client\bin\Release\netstandard2.0\Zero.Game.Shared.dll" "Runtime\lib\Zero.Game.Shared.dll" /Y
-
-pause
