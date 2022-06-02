e4832c6247e16fe7d12c751823eed119a9a1eb4d
Tucker <5263190+Juiix@users.noreply.github.com>
2022-06-01 21:20:56 -0600

    Binary update

---
+++ copy_libs.bat
@@ -0,0 +1,5 @@
+xcopy /s "..\Zero.Gdk\Zero.Game.Client\bin\Release\netstandard2.0\Zero.Game.Client.dll" "Runtime\lib\Zero.Game.Client.dll" /Y
+xcopy /s "..\Zero.Gdk\Zero.Game.Client\bin\Release\netstandard2.0\Zero.Game.Common.dll" "Runtime\lib\Zero.Game.Common.dll" /Y
+xcopy /s "..\Zero.Gdk\Zero.Game.Client\bin\Release\netstandard2.0\Zero.Game.Shared.dll" "Runtime\lib\Zero.Game.Shared.dll" /Y
+
+pause
