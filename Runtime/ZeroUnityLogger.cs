222173ded43fd868753c18d359c0fe1582dbd70f
Tucker <5263190+Juiix@users.noreply.github.com>
2022-09-27 18:10:34 -0600

    Gdk v2 update

--- Runtime/ZeroUnityLogger.cs
+++ Runtime/ZeroUnityLogger.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using UnityEngine;
+using System.Text;
 using Zero.Game.Shared;
 
 namespace Zero.Unity
 {
 	public class ZeroUnityLogger : ILoggingProvider
 	{
-		private class Logged
+		private struct LogEntry
 		{
-            public Logged(LogLevel level, string message, string exception)
+            public LogEntry(LogLevel level, string message, string exception)
             {
                 Level = level;
                 Message = message;
@@ -23,37 +23,58 @@ namespace Zero.Unity
 			public string Exception { get; }
 		}
 
-		private readonly ConcurrentQueue<Logged> _logQueue = new ConcurrentQueue<Logged>();
-
-		public void Update()
-		{
-			while (_logQueue.TryDequeue(out var logged))
-			{
-				InternalLog(logged);
-			}
-		}
-
-		private void InternalLog(Logged logged)
-		{
-			var logFunc = (logged.Level == LogLevel.Error || logged.Level == LogLevel.Critical) ? (Action<string>)Debug.LogError : Debug.Log;
-			if (logged.Exception == null)
-			{
-				logFunc($"[{logged.Level.ToString().ToUpper()}] {logged.Message}");
-			}
-			else
-			{
-				logFunc($"[{logged.Level.ToString().ToUpper()}] {logged.Message}\n{logged.Exception}");
-			}
-		}
+		private readonly ConcurrentQueue<LogEntry> _logQueue = new();
+		private readonly StringBuilder _builder = new();
 
         public void Log(LogLevel logLevel, string message, Exception e)
         {
-			_logQueue.Enqueue(new Logged(logLevel, message, e?.ToString()));
+			_logQueue.Enqueue(new LogEntry(logLevel, message, e?.ToString()));
         }
 
         public void Log(LogLevel logLevel, string format, object[] args, Exception e)
         {
 			Log(logLevel, string.Format(format, args), e);
-        }
-    }
+		}
+
+		public void Update()
+		{
+			while (_logQueue.TryDequeue(out var logged))
+			{
+				InternalLog(ref logged);
+			}
+		}
+
+		private void InternalLog(ref LogEntry logged)
+		{
+			var logFunc = (logged.Level == LogLevel.Error || logged.Level == LogLevel.Critical) ? (Action<string>)UnityEngine.Debug.LogError : UnityEngine.Debug.Log;
+			_builder.Append(GetLogLevelString(logged.Level));
+			_builder.Append(": ");
+			if (logged.Exception == null)
+			{
+				_builder.Append(logged.Message);
+			}
+			else
+			{
+				_builder.Append(logged.Message);
+				_builder.Append("\n");
+				_builder.Append(logged.Exception);
+			}
+			logFunc(_builder.ToString());
+			_builder.Clear();
+		}
+
+		private static string GetLogLevelString(LogLevel logLevel)
+		{
+			return logLevel switch
+			{
+				LogLevel.Trace => "trce",
+				LogLevel.Debug => "dbug",
+				LogLevel.Information => "info",
+				LogLevel.Warning => "warn",
+				LogLevel.Error => "fail",
+				LogLevel.Critical => "crit",
+				_ => "    "
+			};
+		}
+	}
 }
